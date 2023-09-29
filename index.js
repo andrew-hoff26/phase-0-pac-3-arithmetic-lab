b868fe08e9c3d4a496b943791da0535a9e7db54d
andrew <andrew_hoffman262@outlook.com>
2023-09-29 16:02:50 -0600

    define functions to pass all npm tests

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(value1, value2) {
+    return(value1 + value2)
+}
+function subtract(value1, value2) {
+    return(value1 - value2)
+}
+function multiply(a, b) {
+    return(a * b)
+}
+function divide(a, b) {
+    return(a /b)
+}
+function increment(n) {
+    return(n + 1)
+}
+function decrement(n) {
+    return(n - 1)
+}
+function makeInt(n) {
+    const number = parseInt(n,10)
+    return(number)
+}
+function preserveDecimal(n) { 
+    const parsedNumber = parseFloat(n)
+    return(parsedNumber)
+}
